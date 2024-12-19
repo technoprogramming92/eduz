70be54931988268cdb6fc8e4f799b2ce454da3b1
Rachit Trivedi <technoprogramming92@gmail.com>
2024-12-19 16:38:35 +0530

    404-page-added1

--- app/error.tsx
+++ app/error.tsx
@@ -6,10 +6,12 @@ ErrorPage;
 
 export default function Error() {
   return (
-    <ErrorPage
-      code="500"
-      title="Something went wrong!"
-      description="We're experiencing some technical difficulties. Please try again later or contact support if the problem persists."
-    />
+    <>
+      <ErrorPage
+        code="500"
+        title="Something went wrong!"
+        description="We're experiencing some technical difficulties. Please try again later or contact support if the problem persists."
+      />
+    </>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,10 +4,12 @@ ErrorPage;
 
 export default function NotFound() {
   return (
-    <ErrorPage
-      code="404"
-      title="This is not the page you are looking for"
-      description="Sorry, we couldn't find the page you're looking for. Please check the URL in the address bar and try again."
-    />
+    <>
+      <ErrorPage
+        code="404"
+        title="This is not the page you are looking for"
+        description="Sorry, we couldn't find the page you're looking for. Please check the URL in the address bar and try again."
+      />
+    </>
   );
 }
